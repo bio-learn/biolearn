38f28bbf56165cf6a5d45f43ba139f1ec11615fd
Seth <sarudak@gmail.com>
2023-09-14 15:35:16 -0700

    Format

--- biolearn/test/test_other_biomarkers.py
+++ biolearn/test/test_other_biomarkers.py
@@ -7,6 +7,7 @@ import pytest
 
 sample_inputs = load_test_data_file("external/DNAmTestSet.csv")
 
+
 def test_estimate_sex():
     expected = load_test_data_file("expected_estimate_sex_output.csv")
     actual = estimate_sex(sample_inputs)
--- biolearn/util.py
+++ biolearn/util.py
@@ -26,9 +26,11 @@ def get_test_data_file(relative_path):
     )  # build the path to the data file
     return data_file_path
 
+
 def load_test_data_file(relative_path):
     test_sample = pd.read_csv(get_test_data_file(relative_path), index_col=0)
     return test_sample
+
 
 def cached_dowload(url_or_filepath):
     """Downloads the file at a URL and saves it locally. If called again with the same URL it will use the saved file. Returns the local filepath"""
