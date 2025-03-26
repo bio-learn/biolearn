be5645a039d51dabbc8a36da027f9c85903b86c1
Chancen Law <chancenlaw@gmail.com>
2025-03-25 21:01:52 -0400

    Mix EpiTOC1 to random point in model_definitions

--- biolearn/model.py
+++ biolearn/model.py
@@ -245,6 +245,17 @@ model_definitions = {
             "default_imputation": "averaging",
         },
     },
+    "EpiTOC1": {
+        "year": 2016,
+        "species": "Human",
+        "tissue": "Blood",
+        "source": "https://doi.org/10.1186/s13059-016-1064-3",
+        "output": "Stem Cell Division Rate",
+        "model": {
+            "type": "LinearMethylationModel",
+            "file": "EpiTOC1.csv",
+        },
+    },
     "LeeRefinedRobust": {
         "year": 2019,
         "species": "Human",
@@ -491,17 +502,6 @@ model_definitions = {
                 map_ensembl_to_gene(rna_data)
             ),
             "transform": lambda sum: sum + 55.808884324,
-        },
-    },
-    "EpiTOC1": {
-        "year": 2016,
-        "species": "Human",
-        "tissue": "Blood",
-        "source": "https://doi.org/10.1186/s13059-016-1064-3",
-        "output": "Stem Cell Division Rate",
-        "model": {
-            "type": "LinearMethylationModel",
-            "file": "EpiTOC1.csv",
         },
     },
 }
